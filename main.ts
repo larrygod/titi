1dcb4230ae08d77467601431a23bffacb1a852ae
larrygod <103495876+larrygod@users.noreply.github.com>
2022-04-19 11:27:24 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,3 @@
 input.onGesture(Gesture.Shake, function () {
 	
 })
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.clearScreen()
-        basic.showString("t=")
-        basic.showNumber(input.temperature())
-        basic.showString("DEGRES")
-    } else {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-    }
-})
